d9d0099415872d5cee5933e7cc618c3fb720720f
Disha <dishakamble24@gmail.com>
2025-02-11 16:04:20 +0530

    Main Method

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generics Practice !");
+            Console.WriteLine();
+
+        }
+
+    }
+
+}
+

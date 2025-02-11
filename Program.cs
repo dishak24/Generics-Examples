e5e4b34c7f5399a4aaa69c4121f4b46ae33cab58
Disha <dishakamble24@gmail.com>
2025-02-11 16:21:22 +0530

    Main Method

--- Program.cs
+++ Program.cs
@@ -9,9 +9,12 @@ namespace Generics
             Console.WriteLine("Generics Practice !");
             Console.WriteLine();
 
-            CalculatorGenerics.MenuDrivenCalculator(26, 13);
-            CalculatorGenerics.MenuDrivenCalculator(26.56, 13.2);
-
+            MinMax.FindMax(10, 12, 9);
+            MinMax.FindMin(10, 12, 9);
+            MinMax.FindMax(7.9, 9.0, 2.5);
+            MinMax.FindMin(7.9, 9.0, 2.5);
+            MinMax.FindMax('A', 'B', 'c');
+            MinMax.FindMin('A', 'B', 'c');
         }
 
     }

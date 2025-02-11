d81090c6928f4b22715403a2f1fc67009840afcb
Disha <dishakamble24@gmail.com>
2025-02-11 16:27:30 +0530

    Using Generics

---
+++ CompareDifferentDataType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class CompareDifferentDataType
+    {
+        
+        public static void CompareType<T> (T x, T y)
+        {
+            if ((x.ToString().CompareTo(y.ToString())) == 0)
+            {
+                Console.WriteLine($"{x} is equal to {y}");
+            }
+            else if ((x.ToString().CompareTo(y.ToString())) < 0)
+            {
+                Console.WriteLine($"{x} is smaller than {y}");
+            }
+            else
+            {
+                Console.WriteLine($"{x} is greater than {y}");
+            }
+        }
+
+    }   
+}
